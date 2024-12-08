f5068fa446f79f8c4398938ada3ca59c82b1093b
NadimMahmudRishan <resunroy420@gmail.com>
2024-12-08 22:24:10 +0600

    Services section complete.

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,75 @@
-
+import payment from "../assets/payment.png"
+import shipping from "../assets/shipping.png";
+import refund from "../assets/return.png";
+import gift from "../assets/gift.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 const Services = () => {
+      useEffect(() => {
+        AOS.init({
+          offset: 100,
+          durationi: 500,
+          easing: "ease-in-out",
+        });
+        AOS.refresh();
+      }, []);
     return (
-        <div>
-            Services
+      <div className="w-full lg:px-20 px-5 pt-[0px] pb-[80px] grid lg:grid-cols-4 grid-cols-1 justify-center items-center gap-10 ">
+        <div
+          data-aos="zoom-in"
+          data-aos-delay="100"
+          className="flex flex-col justify-center items-center gap-2 "
+        >
+          <img src={shipping} alt="shipping" />
+          <h1 className="text-xl text-black font-semibold ">
+            Worldwide Shipping
+          </h1>
+          <p className="text-[17px] text-gray-500">
+            Global delivery made reliable.
+          </p>
         </div>
+
+        <div
+          data-aos="zoom-in"
+          data-aos-delay="100"
+          className="flex flex-col justify-center items-center gap-2 "
+        >
+          <img src={payment} alt="payment" />
+          <h1 className="text-xl text-black font-semibold ">
+            100% secure payment
+          </h1>
+          <p className="text-[17px] text-gray-500">
+            Secure payment ensures safe transactions.
+          </p>
+        </div>
+
+        <div
+          data-aos="zoom-in"
+          data-aos-delay="100"
+          className="flex flex-col justify-center items-center gap-2 "
+        >
+          <img src={refund} alt="refund" />
+          <h1 className="text-xl text-black font-semibold ">
+            Refund available
+          </h1>
+          <p className="text-[17px] text-gray-500">
+            Refunds offered for easy returns.
+          </p>
+        </div>
+
+        <div
+          data-aos="zoom-in"
+          data-aos-delay="100"
+          className="flex flex-col justify-center items-center gap-2 "
+        >
+          <img src={gift} alt="gift" />
+          <h1 className="text-xl text-black font-semibold ">Wine Gift</h1>
+          <p className="text-[17px] text-gray-500">
+            Wine gifts for special occasions.
+          </p>
+        </div>
+      </div>
     );
 };
 
--- src/sections/Types.jsx
+++ src/sections/Types.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import banner1 from "../assets/banner1.jpg";
 import banner2 from "../assets/banner2.jpg";
 import banner3 from "../assets/banner3.jpg";
+import { useEffect } from "react";
 const Types = () => {
   useEffect(() => {
     AOS.init({
@@ -15,26 +15,59 @@ const Types = () => {
   }, []);
   return (
     <div className="w-full lg:px-20 px-5 py-[80px] grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-10 ">
-      <div data-aos="zoom-in" data-aos-delay='100' className="flex flex-col justify-center items-end gap-6 bg-cover bg-center p-10 rounded-lg " style={{backgroundImage: `url(${banner1})`}}>
-        <h1 className="text-themeYellow border rounded-lg border-themeYellow px-6 py-2 text-lg ">60% off</h1>\
-        <h1 className="text-4xl text-end text-white font-semibold ">
-            Wareless <br />Devices
+      <div
+        data-aos="zoom-in"
+        data-aos-delay="100"
+        className="flex flex-col justify-center items-end gap-6 bg-cover bg-center p-10 rounded-lg "
+        style={{ backgroundImage: `url(${banner1})` }}
+      >
+        <h1 className="text-themeYellow border rounded-lg border-themeYellow px-6 py-2 text-lg ">
+          60% off
         </h1>
-        <button className="bg-themeYellow px-6 py-3 rounded-lg text-black font-semibold ">SHOP NOW</button>
+        \
+        <h1 className="text-4xl text-end text-white font-semibold ">
+          Wareless <br />
+          Devices
+        </h1>
+        <button className="bg-themeYellow px-6 py-3 rounded-lg text-black font-semibold ">
+          SHOP NOW
+        </button>
       </div>
-      <div data-aos="zoom-in" data-aos-delay='100' className="flex flex-col justify-center items-end gap-6 bg-cover bg-center p-10 rounded-lg " style={{backgroundImage: `url(${banner2})`}}>
-        <h1 className="text-themeYellow border rounded-lg border-themeYellow px-6 py-2 text-lg ">60% off</h1>\
-        <h1 className="text-4xl text-end text-white font-semibold ">
-            Wareless <br />Devices
+      <div
+        data-aos="zoom-in"
+        data-aos-delay="100"
+        className="flex flex-col justify-center items-end gap-6 bg-cover bg-center p-10 rounded-lg "
+        style={{ backgroundImage: `url(${banner2})` }}
+      >
+        <h1 className="text-themeYellow border rounded-lg border-themeYellow px-6 py-2 text-lg ">
+          60% off
         </h1>
-        <button className="bg-themeYellow px-6 py-3 rounded-lg text-black font-semibold ">SHOP NOW</button>
+        \
+        <h1 className="text-4xl text-end text-white font-semibold ">
+          Wareless <br />
+          Devices
+        </h1>
+        <button className="bg-themeYellow px-6 py-3 rounded-lg text-black font-semibold ">
+          SHOP NOW
+        </button>
       </div>
-      <div data-aos="zoom-in" data-aos-delay='100' className="flex flex-col justify-center items-end gap-6 bg-cover bg-center p-10 rounded-lg " style={{backgroundImage: `url(${banner3})`}}>
-        <h1 className="text-themeYellow border rounded-lg border-themeYellow px-6 py-2 text-lg ">60% off</h1>\
-        <h1 className="text-4xl text-end text-white font-semibold ">
-            Wareless <br />Devices
+      <div
+        data-aos="zoom-in"
+        data-aos-delay="100"
+        className="flex flex-col justify-center items-end gap-6 bg-cover bg-center p-10 rounded-lg "
+        style={{ backgroundImage: `url(${banner3})` }}
+      >
+        <h1 className="text-themeYellow border rounded-lg border-themeYellow px-6 py-2 text-lg ">
+          60% off
         </h1>
-        <button className="bg-themeYellow px-6 py-3 rounded-lg text-black font-semibold ">SHOP NOW</button>
+        \
+        <h1 className="text-4xl text-end text-white font-semibold ">
+          Wareless <br />
+          Devices
+        </h1>
+        <button className="bg-themeYellow px-6 py-3 rounded-lg text-black font-semibold ">
+          SHOP NOW
+        </button>
       </div>
     </div>
   );
