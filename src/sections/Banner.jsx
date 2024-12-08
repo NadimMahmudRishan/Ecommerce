c94b36457969bb3d513b0d849dae804fe76afa6e
NadimMahmudRishan <resunroy420@gmail.com>
2024-12-09 00:13:04 +0600

    Banner section added.

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -1,8 +1,24 @@
-
+import { useEffect } from "react";
+import deal from "../assets/deal-bg.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const Banner = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      durationi: 500,
+      easing: "ease-in-out",
+    });
+    AOS.refresh();
+  }, []);
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="w-full lg:px-20 px-5 py-[80px]">
+      <div data-aos="zoom-in" data-aos-delay="100" className="w-full h-[300px] rounded-lg bg-cover bg-center flex flex-col justify-center items-center gap-3 " style={{backgroundImage:`url(${deal})`}}>\
+        <h1 className="text-themeYellow text-xl font-semibold">Everyday Shopping</h1>
+        <h1 className="text-white font-bold text-[42px] leading-[50px] text-center ">Deal Of The Day</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
