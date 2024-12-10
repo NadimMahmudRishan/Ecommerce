5efbbc07299c77550185c062e70e9bb9a4e9b6ce
NadimMahmudRishan <resunroy420@gmail.com>
2024-12-10 17:28:49 +0600

    Nav bar chack

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -119,11 +119,11 @@ const Header = () => {
             )}
           </div>
         </div>
-        <div className={`${isMenuOpen ? 'flex' : 'hidden'} w-full h-fit bg-themePurple p-4 absolute top-[80px] left-0`} onClick={closeMenu}>
+        <div className={`${isMenuOpen ? 'flex ' : 'hidden'} w-full h-fit bg-themePurple p-4 absolute top-[80px] left-0`} onClick={closeMenu}>
             <ul>
               {
                 navItems.map(({link,path})=>(
-                  <Link key={path} className="text-white uppercase font-semibold cursor-pointer p-3 rounded-lg hover:bg-themeYellow hover:text-black w-full text-center" to={path} spy={true} offset={-100} smooth={true}>{link}</Link>
+                  <Link key={path} className="text-white uppercase font-semibold cursor-pointer px-1 py-3 rounded-lg hover:bg-themeYellow hover:text-black w-full text-center" to={path} spy={true} offset={-100} smooth={true}>{link}</Link>
                 ))
               }
             </ul>
